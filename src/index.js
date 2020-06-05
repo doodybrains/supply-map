c7c0e1cf0814fe8b72dd8fb4c18a203a904a0cb2
doodybrains <doodybrainz@gmail.com>
2020-06-04 22:40:21 -0400

    add files

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+import csv2geojson from 'csv2geojson';
+import axios from 'axios';
+import config from './config';
+
+mapboxgl.accessToken = config.MAPBOX;
+
+class Application extends React.Component {
+  mapRef = React.createRef();
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: -73.9430786608703,
+      lat: 40.77609485,
+      zoom: 12,
+      locations: null,
+      mb: null
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapRef.current,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    axios.get(config.GOOGLE_SHEET)
+    .then((response) => {
+      this.setState({locations: response.data, mb: map});
+      this.addToMap(this.state.mb);
+    });
+  }
+
+  addToMap(m) {
+    if (this.state.locations) {
+      console.log('loaded', this.state.locations)
+      csv2geojson.csv2geojson(this.state.locations, {
+        latfield: 'Latitude',
+        lonfield: 'Longitude',
+        delimiter: ','
+      }, function (err, data) {
+        if (m) {
+          m.addLayer({
+            'id': 'csvData',
+            'type': 'circle',
+            'source': {
+              'type': 'geojson',
+              'data': data
+            },
+            'paint': {
+              'circle-radius': 5,
+              'circle-color': "green"
+            }
+          });
+        }
+
+      });
+    }
+
+  }
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+      <div>
+        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div ref={this.mapRef} className="absolute top right left bottom" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
